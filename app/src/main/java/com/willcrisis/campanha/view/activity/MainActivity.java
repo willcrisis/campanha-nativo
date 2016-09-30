abb777f9c93c9bc1fc419f3c5618c126610b7c8a
Muryllo Tiraza <m.tiraza@gmail.com>
2016-09-30 17:11:35 -0300

    Implementações   - Compartilhar texto
    Melhorias
      - Minificação do APK gerado

--- app/src/main/java/com/willcrisis/campanha/view/activity/MainActivity.java
+++ app/src/main/java/com/willcrisis/campanha/view/activity/MainActivity.java
@@ -1,15 +1,21 @@
 package com.willcrisis.campanha.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.willcrisis.campanha.R;
 import com.willcrisis.campanha.adapter.SwipeAdapter;
+import com.willcrisis.campanha.model.Capitulo;
+import com.willcrisis.campanha.model.Dia;
+import com.willcrisis.campanha.model.Livro;
+import com.willcrisis.campanha.model.Semana;
+import com.willcrisis.campanha.model.Versiculo;
+import com.willcrisis.campanha.service.SemanaService;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,9 +48,56 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_hoje_compartilhar:
-                Toast.makeText(this, "Compartilhar", Toast.LENGTH_SHORT).show();
+                String textoFormatado = getTextoFormatado();
+                Intent shareIntent = getShareTextIntent(textoFormatado);
+                startActivity(Intent.createChooser(shareIntent, getString(R.string.compartilhar)));
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getTextoFormatado() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        SemanaService service = SemanaService.getInstance(null);
+        Semana semana = service.findSemana(service.getSemana());
+        Dia dia = semana.findDia(service.getDia());
+
+        stringBuilder.append(getString(R.string.texto_semana, semana.id, semana.tema));
+        stringBuilder.append("\n");
+        stringBuilder.append(dia.getNome());
+
+        stringBuilder.append("\n");
+        stringBuilder.append("\n");
+        stringBuilder.append(getString(R.string.atributo, dia.atributo));
+
+        stringBuilder.append("\n");
+        stringBuilder.append("\n");
+        stringBuilder.append(dia.textos);
+
+        for (Livro livro : dia.livros) {
+            stringBuilder.append("\n");
+            stringBuilder.append("\n");
+            stringBuilder.append(livro.nome);
+            stringBuilder.append("\n");
+
+            for (Capitulo capitulo : livro.capitulos) {
+                stringBuilder.append(getString(R.string.texto_capitulo, capitulo.id));
+                stringBuilder.append("\n");
+
+                for (Versiculo versiculo : capitulo.versiculos) {
+                    stringBuilder.append(getString(R.string.texto_versiculo, versiculo.id, versiculo.texto));
+                }
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private Intent getShareTextIntent(String shareText) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareText);
+        return sharingIntent;
     }
 }
